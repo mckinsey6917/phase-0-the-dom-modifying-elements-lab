4b25c6d7c3ee1d916d06c06c8775ee7fa9d81cc3
mckinseyleckenby <leckenbym11@gmail.com>
2021-10-01 16:43:20 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ document.querySelector("main").remove();
 const newHeader = document.createElement('h1');
 newHeader.id = "victory";
 document.body.appendChild(newHeader);
-newHeader.innerHTML = "Kyle is the champion";
+newHeader.innerHTML = "McKinsey is the champion";
